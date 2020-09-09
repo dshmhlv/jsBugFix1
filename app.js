971d34b3cb0060a9a721c1d95480c339b87103a4
dshmhlv <dashakersh1711@gmail.com>
2020-09-09 21:12:44 +0200

    fix JS bug

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
-const ProductName = ['Product 1', 'Product 2', 'Product 3'];
+const ProductName = ['Picture 1', 'Picture 2', 'Picture 3'];
 const ProductImage = ['https://ekit.co.uk/GalleryEntries/ecommerce_solutions_and_services/MedRes_Product-presentation-2.jpg', 'https://ekit.co.uk/GalleryEntries/ecommerce_solutions_and_services/MedRes_Product-presentation-2.jpg', 'https://ekit.co.uk/GalleryEntries/ecommerce_solutions_and_services/MedRes_Product-presentation-2.jpg']
 
-for(let i =0; i < ProductName.length; i++)
+
+for(let i = 0; i < ProductName.length; i++)
 {
     console.log("Test");
-    document.getElementById('header').innerHTML = document.getElementById('header').innerText + "<li>" + ProductName[i] + "<img src="+ ProductImage[i] + ">"+ "</li>";
+    document.getElementById('header').innerHTML = document.getElementById('header').innerHTML + "<li>" + ProductName[i] + "<img src="+ ProductImage[i] + ">"+ "</li>";
 }
+
+
